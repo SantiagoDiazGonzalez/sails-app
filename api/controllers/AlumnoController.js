39fa3646e6ca21ac56b57065661851ee219b203e
SantiagoDiazGonzalez <sandiazdiez@gmail.com>
2018-04-23 19:50:49 -0300

    se agrego test con models, views y controllers

---
+++ api/controllers/AlumnoController.js
@@ -0,0 +1,15 @@
+/**
+ * AlumnoController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  list: async function (req, res) {
+
+    var alumnos = await Alumno.find({});
+
+    res.view( 'pages/test', {a:JSON.stringify(alumnos)} );
+  }
+};
